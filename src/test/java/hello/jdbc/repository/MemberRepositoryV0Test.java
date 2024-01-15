b6d305103cc1d435376040e27897aaa0fe35d5fb
MOONJJJJJ <yangmoonjj5@gmail.com>
2024-01-15 11:25:06 +0900

    1 - JDBC 개발 - 수정, 삭제

--- src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -4,18 +4,30 @@ import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import java.sql.SQLException;
+import java.util.NoSuchElementException;
+
 @Slf4j
 class MemberRepositoryV0Test {
     MemberRepositoryV0 repository = new MemberRepositoryV0();
     @Test
     void crud() throws SQLException {
         //save
-        Member member = new Member("memberV20", 230000);
+        Member member = new Member("memberV30", 30000);
         repository.save(member);
 
         //find
         Member findMember = repository.findById(member.getMemberId());
         log.info("findMember={}",findMember);
         Assertions.assertThat(findMember).isEqualTo(member);
+
+        //update
+        repository.update(member.getMemberId(), 1000);
+        Member updateMember = repository.findById(member.getMemberId());
+        Assertions.assertThat(updateMember.getMoney()).isEqualTo(1000);
+
+        //delete
+        repository.delete(member.getMemberId());
+        Assertions.assertThatThrownBy(() -> repository.findById(member.getMemberId()))
+                .isInstanceOf(NoSuchElementException.class);
     }
 }
