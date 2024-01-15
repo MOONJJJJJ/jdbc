1dae6c9a0817f9a0edc0a0704a482c0003de9303
MOONJJJJJ <yangmoonjj5@gmail.com>
2024-01-15 23:13:41 +0900

    2 - DataSource 적용

--- src/test/java/hello/jdbc/repository/MemberRepositoryV1Test.java
+++ src/test/java/hello/jdbc/repository/MemberRepositoryV1Test.java
@@ -1,29 +1,30 @@
 package hello.jdbc.repository;
-
+import com.zaxxer.hikari.HikariDataSource;
 import hello.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-
 import java.sql.SQLException;
 import java.util.NoSuchElementException;
-
 import static hello.jdbc.connection.ConnectionConst.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
-
 @Slf4j
 class MemberRepositoryV1Test {
     MemberRepositoryV1 repository;
-
     @BeforeEach
-    void beforeEach(){
-        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL,USERNAME,PASSWORD);
+    void beforeEach() throws Exception {
+        //기본 DriverManager - 항상 새로운 커넥션 획득
+        //DriverManagerDataSource dataSource =
+        // new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        //커넥션 풀링: HikariProxyConnection -> JdbcConnection
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
         repository = new MemberRepositoryV1(dataSource);
     }
-
     @Test
     void crud() throws SQLException, InterruptedException {
         log.info("start");
@@ -42,5 +43,4 @@ class MemberRepositoryV1Test {
         assertThatThrownBy(() -> repository.findById(member.getMemberId()))
                 .isInstanceOf(NoSuchElementException.class);
     }
-
 }
